c3f8407448ace38ae90188f4ee6a98ab4de2b59a
phact <estevezsebastian@gmail.com>
2025-03-10 16:26:37 -0400

    .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-hi
+# mcp-browser-use-tools
+
+This package vendors a subset of [browser-use](https://github.com/browser-use/browser-use) with minimal dependencies exposed as an mcp-server.
+
+Supports the following tools:
+
+ - done
+ - search_google
+ - go_to_url
+ - go_back
+ - wait
+ - click_element
+ - input_text
+ - switch_tab
+ - open_tab
+ - extract_content
+ - scroll_down
+ - scroll_up
+ - send_keys
+ - scroll_to_text
+ - get_dropdown_options
+ - select_dropdown_option
--- pull_subtree.sh
+++ pull_subtree.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-GIT_LFS_SKIP_SMUDGE=1 git subtree pull --prefix=browser-use https://github.com/browser-use/browser-use main --squash
-cp -R browser-use/browser_use/browser/ src/browser/
-cp browser-use/browser_use/controller/views.py src/controller/views.py
-cp -R browser-use/browser_use/dom/ src/dom/
+#GIT_LFS_SKIP_SMUDGE=1 git subtree pull --prefix=browser-use https://github.com/browser-use/browser-use main --squash
+cp -R browser-use/browser_use/browser/* src/browser_use/browser/
+cp browser-use/browser_use/controller/views.py src/browser_use/controller/views.py
+cp -R browser-use/browser_use/dom/* src/browser_use/dom/
+cp -R browser-use/browser_use/utils.py src/browser_use/
