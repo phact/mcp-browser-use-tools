8248cc77cf4a51dab48ab01d0b464b3636073df4
phact <estevezsebastian@gmail.com>
2025-03-10 15:24:54 -0400

    pull_subtree.sh

--- pull_subtree.sh
+++ pull_subtree.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 GIT_LFS_SKIP_SMUDGE=1 git subtree pull --prefix=browser-use https://github.com/browser-use/browser-use main --squash
+cp browser-use/browser_use/browser/*.py src/minimal_browser/
+cp browser-use/browser_use/controller/views.py src/controller/views.py
